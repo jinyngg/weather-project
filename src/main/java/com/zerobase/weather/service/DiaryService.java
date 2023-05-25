32570ddf51f57e0425858c05c984e01e1cde21ef
jyjang <jinyngg@gmail.com>
2023-05-26 02:16:18 +0900

    feat : 로그 설정 추가 (#10)
    1. 로그 config 추가
    2. create 메소드 로깅 추가

--- src/main/java/com/zerobase/weather/service/DiaryService.java
+++ src/main/java/com/zerobase/weather/service/DiaryService.java
@@ -1,6 +1,7 @@
 
 package com.zerobase.weather.service;
 
+import com.zerobase.weather.WeatherApplication;
 import com.zerobase.weather.domain.DateWeather;
 import com.zerobase.weather.domain.Diary;
 import com.zerobase.weather.repository.DateWeatherRepository;
@@ -10,6 +11,8 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -34,6 +37,8 @@ public class DiaryService {
 
     private final DiaryRepository diaryRepository;
     private final DateWeatherRepository dateWeatherRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(WeatherApplication.class);
 
     /**
      * @Scheduled(cron = "0 0 1 * * *") => 매일 새벽 1시에 saveWeatherDate() 실행
@@ -73,6 +78,8 @@ public class DiaryService {
 
     @Transactional(isolation = Isolation.SERIALIZABLE)
     public void createDiary(LocalDate date, String text) {
+
+        logger.info("=== started to create diary ===");
         
         // 날씨 데이터 가져오기(DB에서 가져오기)
         DateWeather dateWeather = getDateWeather(date);
@@ -84,6 +91,8 @@ public class DiaryService {
         nowDiary.setDate(date);
 
         diaryRepository.save(nowDiary);
+
+        logger.info("=== end to create diary ===");
     }
 
     private DateWeather getDateWeather(LocalDate date) {
@@ -152,6 +161,7 @@ public class DiaryService {
 
     @Transactional(readOnly = true)
     public List<Diary> readDiary(LocalDate date) {
+        logger.debug("=== read diary ===");
         return diaryRepository.findAllByDate(date);
     }
 
