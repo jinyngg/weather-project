59c31aa1ef6543ada47d33ab10b3514e39584934
jyjang <jinyngg@gmail.com>
2023-06-06 01:48:23 +0900

    refactor : exception console 로그 log로 변경 (#16)

--- src/main/java/com/zerobase/weather/config/GlobalExceptionHandler.java
+++ src/main/java/com/zerobase/weather/config/GlobalExceptionHandler.java
@@ -1,17 +1,20 @@
 package com.zerobase.weather.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
     public Exception handlerAllException() {
-        System.out.println("error from GlobalException");
+
+        log.error("error from GlobalException");
         return new Exception();
     }
 }
